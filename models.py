f400384cbf008f231ab81b2b9385ec5ae765c4ee
Jack Scantlebury <jack.scantlebury@gmail.com>
2021-03-04 18:27:50 +0000

    Added LieTransformer

--- models.py
+++ models.py
@@ -8,10 +8,14 @@ import wandb
 from egnn_pytorch import EGNN
 from egnn_pytorch.egnn_pytorch import fourier_encode_dist, exists
 from einops import rearrange, repeat
-from lie_conv.lieConv import LieConv, GlobalPool, BottleBlock, Swish
-from lie_conv.lieGroups import SE3
+from eqv_transformer.eqv_attention import EquivariantTransformerBlock, \
+    GlobalPool
+from eqv_transformer.utils import Swish
+from lie_conv.lieConv import BottleBlock, LieConv
 from lie_conv.masked_batchnorm import MaskBatchNormNd
-from lie_conv.utils import Expression, Pass
+from lie_conv.utils import Expression
+from lie_transformer_pytorch.lie_transformer_pytorch import Pass
+from lie_transformer_pytorch.se3 import SE3
 from torch import nn, einsum
 from torch.distributions.multivariate_normal import MultivariateNormal as MVN
 
@@ -48,7 +52,7 @@ class PointNeuralNetwork(nn.Module):
 
         self.build_net(**model_kwargs)
         self.optimiser = torch.optim.Adam(
-            self.parameters(), lr=self.lr, weight_decay=1e-4)
+            self.parameters(), lr=self.lr, weight_decay=1e-6)
 
         self.cuda()
 
@@ -319,6 +323,18 @@ class PointNeuralNetwork(nn.Module):
         return sum(
             [torch.numel(t) for t in self.parameters() if t.requires_grad])
 
+    @staticmethod
+    def xavier_init(m):
+        if isinstance(m, (nn.Linear, nn.Conv2d)):
+            nn.init.xavier_normal_(m.weight)
+            if m.bias is not None:
+                m.bias.data.fill_(0)
+
+        elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d)):
+            m.weight.data.fill_(1)
+            if m.bias is not None:
+                m.bias.data.fill_(0)
+
 
 class LieResNet(PointNeuralNetwork):
     """Generic ResNet architecture from https://arxiv.org/abs/2002.12880"""
@@ -451,6 +467,114 @@ class EnResBlock(nn.Module):
         return new_coords, new_values, mask
 
 
+class EquivariantTransformer(PointNeuralNetwork):
+    """Adapted from https://github.com/anonymous-code-0/lie-transformer"""
+
+    def _get_y_true(self, y):
+        return y.cuda()
+
+    def _process_inputs(self, x):
+        return tuple([ten.cuda() for ten in x] + [None])
+
+    def build_net(self, dim_input, dim_output, dim_hidden, num_layers,
+                  num_heads, global_pool=True, global_pool_mean=True,
+                  group=SE3(0.2), liftsamples=1, block_norm="layer_pre",
+                  output_norm="none", kernel_norm="none", kernel_type="mlp",
+                  kernel_dim=16, kernel_act="swish", mc_samples=0, fill=1.0,
+                  attention_fn="norm_exp", feature_embed_dim=None,
+                  max_sample_norm=None, lie_algebra_nonlinearity=None):
+
+        if isinstance(dim_hidden, int):
+            dim_hidden = [dim_hidden] * (num_layers + 1)
+
+        if isinstance(num_heads, int):
+            num_heads = [num_heads] * num_layers
+
+        attention_block = lambda dim, n_head: EquivariantTransformerBlock(
+            dim, n_head, group, block_norm=block_norm, kernel_norm=kernel_norm,
+            kernel_type=kernel_type, kernel_dim=kernel_dim,
+            kernel_act=kernel_act, mc_samples=mc_samples, fill=fill,
+            attention_fn=attention_fn, feature_embed_dim=feature_embed_dim,
+        )
+
+        activation_fn = {
+            "swish": Swish,
+            "relu": nn.ReLU,
+            "softplus": nn.Softplus,
+        }
+
+        if output_norm == "batch":
+            norm1 = nn.BatchNorm1d(dim_hidden[-1])
+            norm2 = nn.BatchNorm1d(dim_hidden[-1])
+            norm3 = nn.BatchNorm1d(dim_hidden[-1])
+        elif output_norm == "layer":
+            norm1 = nn.LayerNorm(dim_hidden[-1])
+            norm2 = nn.LayerNorm(dim_hidden[-1])
+            norm3 = nn.LayerNorm(dim_hidden[-1])
+        elif output_norm == "none":
+            norm1 = nn.Sequential()
+            norm2 = nn.Sequential()
+            norm3 = nn.Sequential()
+        else:
+            raise ValueError(f"{output_norm} is not a valid norm type.")
+
+        self.net = nn.Sequential(
+            Pass(nn.Linear(dim_input, dim_hidden[0]), dim=1),
+            *[
+                attention_block(dim_hidden[i], num_heads[i])
+                for i in range(num_layers)
+            ],
+            GlobalPool(mean=global_pool_mean)
+            if global_pool
+            else Expression(lambda x: x[1]),
+            nn.Sequential(
+                norm1,
+                activation_fn[kernel_act](),
+                nn.Linear(dim_hidden[-1], dim_hidden[-1]),
+                norm2,
+                activation_fn[kernel_act](),
+                nn.Linear(dim_hidden[-1], dim_hidden[-1]),
+                norm3,
+                activation_fn[kernel_act](),
+                nn.Linear(dim_hidden[-1], dim_output),
+            ),
+        )
+
+        self.group = group
+        self.liftsamples = liftsamples
+        self.max_sample_norm = max_sample_norm
+
+        self.lie_algebra_nonlinearity = lie_algebra_nonlinearity
+        if lie_algebra_nonlinearity is not None:
+            if lie_algebra_nonlinearity == "tanh":
+                self.lie_algebra_nonlinearity = nn.Tanh()
+            else:
+                raise ValueError(
+                    f"{lie_algebra_nonlinearity} is not a supported nonlinearity"
+                )
+
+    def forward(self, input):
+        if self.max_sample_norm is None:
+            lifted_data = self.group.lift(input, self.liftsamples)
+        else:
+            lifted_data = [
+                torch.tensor(self.max_sample_norm * 2, device=input[0].device),
+                0,
+                0,
+            ]
+            while lifted_data[0].norm(dim=-1).max() > self.max_sample_norm:
+                lifted_data = self.group.lift(input, self.liftsamples)
+
+        if self.lie_algebra_nonlinearity is not None:
+            lifted_data = list(lifted_data)
+            pairs_norm = lifted_data[0].norm(dim=-1) + 1e-6
+            lifted_data[0] = lifted_data[0] * (
+                    self.lie_algebra_nonlinearity(pairs_norm / 7) / pairs_norm
+            ).unsqueeze(-1)
+
+        return self.net(lifted_data)
+
+
 class EnResNet(PointNeuralNetwork):
     """ResNet for E(n)Transformer"""
 
@@ -486,12 +610,7 @@ class EnResNet(PointNeuralNetwork):
             GlobalPool(mean=mean) if pool else Expression(lambda x: x[1])
         ])
 
-        def init_weights(m):
-            if type(m) == nn.Linear:
-                torch.nn.init.xavier_uniform(m.weight)
-                m.bias.data.fill_(0.01)
-
-        self.apply(init_weights)
+        self.apply(self.xavier_init)
         print('Params:', self.param_count)
 
 
--- point_vs.py
+++ point_vs.py
@@ -20,7 +20,8 @@ from pathlib import PosixPath
 
 import torch
 import yaml
-from lie_conv.lieGroups import SE3
+# from lie_conv.lieGroups import SE3
+from lie_transformer_pytorch.se3 import SE3
 from torch.utils.data import DataLoader
 
 from acs import utils
@@ -28,7 +29,7 @@ from active_learning import active_learning
 from data_loaders import LieConvDataset, SE3TransformerDataset, \
     multiple_source_dataset
 from models import LieResNet, BayesianPointNN, LieFeatureExtractor, EnResNet, \
-    EnFeatureExtractor
+    EnFeatureExtractor, EquivariantTransformer
 
 try:
     import wandb
@@ -169,9 +170,10 @@ if __name__ == '__main__':
     save_path = args.save_path.expanduser()
     save_path.mkdir(parents=True, exist_ok=True)
 
-    if args.model in ['lieconv', 'al_lieconv', 'entransformer']:
+    if args.model in (
+            'lieconv', 'al_lieconv', 'entransformer', 'lietransformer'):
         ds_class = LieConvDataset
-    elif args.model in ['entransformer', 'al_entransformer']:
+    elif args.model in ('entransformer', 'al_entransformer'):
         ds_class = LieConvDataset
     elif args.model == 'se3transformer':
         raise NotImplementedError('se3transformer has been removed')
@@ -213,6 +215,30 @@ if __name__ == '__main__':
         'pool': True
     }
 
+    lietransformer_model_kwargs = {
+        'dim_input': 12,
+        'dim_output': 2,
+        'dim_hidden': args.channels,  # == 32
+        'num_layers': args.layers,  # == 6
+        'num_heads': 8,
+        'global_pool': True,
+        'global_pool_mean': True,
+        'group': SE3(0.2),
+        'liftsamples': args.liftsamples,  # == 1
+        'block_norm': "layer_pre",
+        'output_norm': "none",
+        'kernel_norm': "none",
+        'kernel_type': "mlp",
+        'kernel_dim': 16,
+        'kernel_act': "swish",
+        'mc_samples': 4,
+        'fill': 1.0,
+        'attention_fn': "norm_exp",
+        'feature_embed_dim': None,
+        'max_sample_norm': None,
+        'lie_algebra_nonlinearity': None,
+    }
+
     with open(save_path / 'model_args.yaml', 'w') as f:
         yaml.dump(lieconv_model_kwargs, f)
     with open(save_path / 'cmd_args.yaml', 'w') as f:
@@ -230,6 +256,21 @@ if __name__ == '__main__':
         'project': args.wandb_project, 'allow_val_change': True,
         'config': vars(args)
     }
+
+    if args.model == 'lietransformer':
+        model = EquivariantTransformer(
+            save_path, args.learning_rate, **lietransformer_model_kwargs)
+        if args.load_weights is not None:
+            model.load(args.load_weights.expanduser())
+        model.save_path = save_path
+        if args.wandb_project is not None:
+            wandb.init(**wandb_init_kwargs)
+            if args.wandb_run is not None:
+                wandb.run.name = args.wandb_run
+            wandb.watch(model)
+        model.optimise(train_dl, epochs=args.epochs)
+        if test_dl is not None:
+            model.test(test_dl)
 
     if args.model == 'lieconv':
         model = LieResNet(save_path, args.learning_rate, **lieconv_model_kwargs)
