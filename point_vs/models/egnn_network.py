c320ebf3a955a3f37603e96e6a541a102de76194
Jack Scantlebury <jack.scantlebury@gmail.com>
2021-10-07 11:14:58 +0100

    Added soft edges to EGNN...

--- point_vs/models/egnn_network.py
+++ point_vs/models/egnn_network.py
@@ -1,4 +1,3 @@
-import torch
 from egnn_pytorch import EGNN as EGNNLayer
 from egnn_pytorch.egnn_pytorch import SiLU, exists
 from eqv_transformer.utils import GlobalPool
@@ -98,7 +97,7 @@ class EGNN(PointNeuralNetwork):
         egnn = lambda: layer_class(
             dim=k, m_dim=m_dim, norm_coors=norm_coords, norm_feats=norm_feats,
             dropout=dropout, fourier_features=fourier_features,
-            num_nearest_neighbors=nbhd, init_eps=1e-2)
+            num_nearest_neighbors=nbhd, init_eps=1e-2, soft_edges=True)
 
         return nn.Sequential(
             Pass(nn.Linear(dim_input, k), dim=1),
@@ -109,66 +108,3 @@ class EGNN(PointNeuralNetwork):
 
     def forward(self, x):
         return self.layers(x)
-
-    def _get_min_max(self, grads=True):
-        """For debugging: print min and max value in each layer."""
-
-        def obtain(obj):
-            return obj.grad if grads else obj
-
-        for i in range(3):
-            print()
-        network = self.layers
-        res = ''
-        min_ = 1e7
-        max_ = -1e7
-        min_abs = 1e7
-        for layer in network:
-            if isinstance(layer, nn.Linear):
-                try:
-                    min_ = min(min_, float(torch.min(obtain(layer.weight))))
-                    max_ = max(max_, float(torch.max(obtain(layer.weight))))
-                    min_abs = min(min_abs,
-                                  float(torch.min(torch.abs(obtain(layer.weight)))))
-                    res += 'Linear: {0}, {1} {2}\n'.format(
-                        float(torch.min(obtain(layer.weight))),
-                        float(torch.max(obtain(layer.weight))),
-                        float(torch.min(torch.abs(obtain(layer.weight)))))
-                except TypeError:
-                    continue
-            if isinstance(layer, Pass):
-                if isinstance(layer.module, nn.Linear):
-                    try:
-                        min_ = min(min_, float(torch.min(obtain(layer.module.weight))))
-                        max_ = max(max_, float(torch.max(obtain(layer.module.weight))))
-                        min_abs = min(min_abs, float(
-                            torch.min(torch.abs(obtain(layer.module.weight)))))
-                        res += 'Linear:{0} {1} {2}\n'.format(
-                            float(torch.min(obtain(layer.module.weight))),
-                            float(torch.max(obtain(layer.module.weight))),
-                            float(torch.min(torch.abs(obtain(layer.module.weight)))))
-                    except TypeError:
-                        continue
-            elif isinstance(layer, EGNNPass):
-                layer = layer.egnn
-                for network_type, network_name in zip(
-                        (layer.edge_mlp, layer.node_mlp, layer.coors_mlp),
-                        ('EGNN-edge', 'EGNN-node', 'EGNN-coors')):
-                    for sublayer in network_type:
-                        if isinstance(sublayer, nn.Linear):
-                            try:
-                                max_ = max(max_, float(
-                                    torch.max(obtain(sublayer.weight))))
-                                min_ = min(min_, float(
-                                    torch.min(obtain(sublayer.weight))))
-                                min_abs = min(min_abs, float(
-                                    torch.min(torch.abs(obtain(sublayer.weight)))))
-                                res += network_name + ': {0} {1} {2}\n'.format(
-                                    float(torch.min(sublayer.weight)),
-                                    float(torch.max(sublayer.weight)),
-                                    float(
-                                        torch.min(
-                                            torch.abs(obtain(sublayer.weight)))))
-                            except TypeError:
-                                res += 'NONE_LAYER\n'
-        return res[:-1], min_, max_, min_abs
