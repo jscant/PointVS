aeb49e43d1899c9f3d68a297f7f49912e5195f48
Jack Scantlebury <jack.scantlebury@gmail.com>
2021-10-23 18:20:43 +0100

    Unit tests for data loaders

--- test/test_class_instantiation.py
+++ test/test_class_instantiation.py
@@ -2,7 +2,9 @@ from point_vs.models.geometric.egnn_lucid import PygLucidEGNN
 from point_vs.models.geometric.egnn_satorras import SartorrasEGNN
 from point_vs.models.vanilla.lie_conv import LieResNet
 from point_vs.models.vanilla.lie_transformer import EquivariantTransformer
-from .setup_and_params import MODEL_KWARGS, setup
+from point_vs.preprocessing.data_loaders import PointCloudDataset, \
+    PygPointCloudDataset
+from .setup_and_params import MODEL_KWARGS, setup, DATALOADER_KWARGS
 
 dump_path = setup()
 
@@ -25,3 +27,13 @@ def test_lietransformer_instantiation():
 def test_lieconv_instantiation():
     model = LieResNet(dump_path, 0, 0, None, None, **MODEL_KWARGS)
     assert model.save_path == dump_path
+
+
+def test_vanilla_data_loader_instantiation():
+    test_dl = PygPointCloudDataset(dump_path, **DATALOADER_KWARGS)
+    assert len(test_dl) == 1
+
+
+def test_pyg_data_loader_instantiation():
+    test_dl = PointCloudDataset(dump_path, **DATALOADER_KWARGS)
+    assert len(test_dl) == 1
