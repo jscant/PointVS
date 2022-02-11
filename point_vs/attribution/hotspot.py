6fe725868e94f9f5647c76f710f00f2446ca8b18
Jack Scantlebury <jack.scantlebury@gmail.com>
2022-02-11 12:39:03 +0000

    Added vdw radii info to hotspot.py

--- point_vs/attribution/hotspot.py
+++ point_vs/attribution/hotspot.py
@@ -12,6 +12,22 @@ AA_TRIPLET_CODES = {'ILE', 'GLU', 'CYS', 'TRP', 'ALA', 'PRO', 'PHE', 'ASN',
                     'GLY', 'THR', 'ARG', 'MET', 'HIS', 'VAL', 'GLN', 'TYR',
                     'LYS', 'LEU', 'SER', 'ASP'}
 
+VDW_RADII = {1: 1.1, 2: 1.4, 3: 1.82, 4: 1.53, 5: 1.92, 6: 1.7, 7: 1.55,
+             8: 1.52, 9: 1.47, 10: 1.54, 11: 2.27, 12: 1.73, 13: 1.84, 14: 2.1,
+             15: 1.8, 16: 1.8, 17: 1.75, 18: 1.88, 19: 2.75, 20: 2.31, 21: 2.15,
+             22: 2.11, 23: 2.07, 24: 2.06, 25: 2.05, 26: 2.04, 27: 2.0,
+             28: 1.97, 29: 1.96, 30: 2.01, 31: 1.87, 32: 2.11, 33: 1.85,
+             34: 1.9, 35: 1.85, 36: 2.02, 37: 3.03, 38: 2.49, 39: 2.32,
+             40: 2.23, 41: 2.18, 42: 2.17, 43: 2.16, 44: 2.13, 45: 2.1, 46: 2.1,
+             47: 2.11, 48: 2.18, 49: 1.93, 50: 2.17, 51: 2.06, 52: 2.06,
+             53: 1.98, 54: 2.16, 55: 3.43, 56: 2.68, 57: 2.43, 58: 2.42,
+             59: 2.4, 60: 2.39, 61: 2.38, 62: 2.36, 63: 2.35, 64: 2.34,
+             65: 2.33, 66: 2.31, 67: 2.3, 68: 2.29, 69: 2.27, 70: 2.26,
+             71: 2.24, 72: 2.23, 73: 2.22, 74: 2.18, 75: 2.16, 76: 2.16,
+             77: 2.13, 78: 2.13, 79: 2.14, 80: 2.23, 81: 1.96, 82: 2.02,
+             83: 2.07, 84: 1.97, 85: 2.02, 86: 2.2, 87: 3.48, 88: 2.83,
+             89: 2.47, 90: 2.45, 91: 2.43, 92: 2.41, 93: 2.39, 94: 2.4}
+
 
 def find_identifier(coords_to_identifier, coords):
     """Find the unique PDB atom identifier given the atom's coordinates.
@@ -156,10 +172,11 @@ def scores_to_pharmacophore_df(reference_pdb, atom_scores):
         and score, sorted by score, where:
 
             x, y and z are cartesian coodinates of the pharmacophores
+            vdw is the van der waals radius of the original protein atom
             score is the attention score of the atom at that position
             smina_type is the sminatype of the atom in the reference structure
                 (for example, AromaticCarbonXSHydrophobe)
-            pharmacophore is one of 'hydrophobic', 'hbda', 'hba', 'hbd', 'none'.
+            pharmacophore is one of 'hydrophobic', 'hbda', 'hba', 'hbd', 'none'
     """
 
     def get_pharmacophore(atom):
@@ -185,12 +202,14 @@ def scores_to_pharmacophore_df(reference_pdb, atom_scores):
     pdb_parser = StructuralFileParser('receptor')
     mol = pdb_parser.read_file(reference_pdb)[0]
     xs, ys, zs = [], [], []
-    scores, pharmacophores, smina_types = [], [], []
+    scores, pharmacophores, smina_types, vdw = [], [], [], []
     for atom in mol:
         if atom.OBAtom.GetResidue() is None or \
                 atom.OBAtom.GetResidue().GetName() not in AA_TRIPLET_CODES or \
                 atom.atomicnum == 1:
             continue
+
+        vdw.append(VDW_RADII[atom.atomicnum])
         identifier = find_identifier(coords_to_atom_id, atom.coords)
         score = id_to_score_map.get(identifier, 0)
         smina_type, pharmacophore = get_pharmacophore(atom)
@@ -201,10 +220,12 @@ def scores_to_pharmacophore_df(reference_pdb, atom_scores):
         pharmacophores.append(pharmacophore)
         smina_types.append(smina_type)
         scores.append(score)
+
     df = pd.DataFrame({
         'x': xs,
         'y': ys,
         'z': zs,
+        'vdw_radius': vdw,
         'smina_type': smina_types,
         'pharmacophore': pharmacophores,
         'score': scores
@@ -241,11 +262,14 @@ def pharmacophore_df_to_mols(df, cutoff=0, include_donor_acceptors=False):
         mol = Chem.MolFromSmiles(smiles)
         conf = Chem.Conformer(mol.GetNumAtoms())
 
+        vdw_str = []
         for idx, row in filtered_df.iterrows():
             conf.SetAtomPosition(idx, list(row[['x', 'y', 'z']]))
+            vdw_str.append(str(row['vdw_radius'] / 100))
 
         conf.SetId(0)
         mol.AddConformer(conf)
+        mol.SetProp('vdw', '\n'.join(vdw_str))
         res.append(mol)
     return tuple(res)
 
