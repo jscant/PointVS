f0082195770964db6d36133b945870b4094b1156
Jack Scantlebury <jack.scantlebury@gmail.com>
2022-06-19 11:46:50 +0200

    Unit test for egnn-encoder

--- test/test_invariance.py
+++ test/test_invariance.py
@@ -1,14 +1,25 @@
 import pytest
-from torch import sigmoid
-
 from point_vs.models.geometric.egnn_lucid import PygLucidEGNN
 from point_vs.models.geometric.egnn_satorras import SartorrasEGNN
 from point_vs.models.vanilla.lie_conv import LieResNet
 from point_vs.models.vanilla.lie_transformer import EquivariantTransformer
+from torch import sigmoid
+
 from .setup_and_params import MODEL_KWARGS, ORIGINAL_COORDS, FEATS, MASK, \
     EGNN_EPS, LIFT_EPS, ROTATED_COORDS, ORIGINAL_GRAPH, setup, ROTATED_GRAPH
 
 dump_path = setup()
+
+
+def test_satorras_egnn_with_transformer_encoder_invariance():
+    model_kwargs_copy = MODEL_KWARGS.copy()
+    model_kwargs_copy['transformer_encoder'] = True
+    model = SartorrasEGNN(
+        dump_path, 0, 0, None, None, **model_kwargs_copy).cuda().eval()
+
+    unrotated_result = float(sigmoid(model(ORIGINAL_GRAPH)))
+    rotated_result = float(sigmoid(model(ROTATED_GRAPH)))
+    assert unrotated_result == pytest.approx(rotated_result, abs=EGNN_EPS)
 
 
 def test_lucid_egnn_invariance():
