5370ff697ecaa5e64d0cfede2b5e0457139cd1af
Jack Scantlebury <jack.scantlebury@gmail.com>
2022-06-19 20:48:37 +0200

    Fixed large graphs

--- point_vs/models/geometric/pnn_geometric_base.py
+++ point_vs/models/geometric/pnn_geometric_base.py
@@ -94,7 +94,7 @@ class PNNGeometricBase(PointNeuralNetworkBase):
         total_nodes, k = feats.shape
         row, col = edges
         if self.transformer_encoder:
-            max_nodes = 256
+            max_nodes = 300
             bs = int(torch.max(batch)) + 1
             feats_ = torch.zeros(bs, max_nodes, k)
             graph_sizes = torch.zeros(bs, dtype=torch.int32)
@@ -102,7 +102,7 @@ class PNNGeometricBase(PointNeuralNetworkBase):
             for i in range(bs):
                 node_values = feats[torch.where(batch == i)]
                 graph_size = min(node_values.size(0), max_nodes)
-                feats_[i, :graph_size, :] = node_values
+                feats_[i, :graph_size, :] = node_values[:max_nodes]
                 graph_sizes[i] = graph_size
                 mask[i, :graph_size] = 1
             feats = feats_.cuda()
