a99ec498d0ff7324200f1308cfc3ee8f49f8261e
jscant <jack.scantlebury@gmail.com>
2022-12-02 13:38:22 +0100

    Switch to using TemporaryDirectory.

--- test/test_class_instantiation.py
+++ test/test_class_instantiation.py
@@ -1,32 +1,38 @@
 """Check if important classes are correctly instantiated."""
+
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
 from point_vs.models.geometric.egnn_lucid import PygLucidEGNN
 from point_vs.models.geometric.egnn_satorras import SartorrasEGNN
 from point_vs.preprocessing.data_loaders import PointCloudDataset, \
     PygPointCloudDataset
-from .setup_and_params import MODEL_KWARGS, setup, DATALOADER_KWARGS
-
-dump_path = setup()
+from .setup_and_params import MODEL_KWARGS, DATALOADER_KWARGS
 
 
 def test_sartorras_egnn_instantiation():
     """Check if satorras-egnn is instantiated."""
-    model = SartorrasEGNN(dump_path, 0, 0, None, None, **MODEL_KWARGS)
-    assert model.save_path == dump_path
+    with TemporaryDirectory() as tmpdir:
+        model = SartorrasEGNN(Path(tmpdir), 0, 0, None, None, **MODEL_KWARGS)
+        assert str(model.save_path) == tmpdir
 
 
 def test_lucid_egnn_instantiation():
     """Check if lucid-egnn is instantiated."""
-    model = PygLucidEGNN(dump_path, 0, 0, None, None, **MODEL_KWARGS)
-    assert model.save_path == dump_path
+    with TemporaryDirectory() as tmpdir:
+        model = PygLucidEGNN(Path(tmpdir), 0, 0, None, None, **MODEL_KWARGS)
+        assert str(model.save_path) == tmpdir
 
 
 def test_vanilla_data_loader_instantiation():
     """Check if data_loader is instantiated."""
-    test_dl = PygPointCloudDataset(dump_path, **DATALOADER_KWARGS)
-    assert len(test_dl) == 1
+    with TemporaryDirectory() as tmpdir:
+        test_dl = PygPointCloudDataset(Path(tmpdir), **DATALOADER_KWARGS)
+        assert len(test_dl) == 1
 
 
 def test_pyg_data_loader_instantiation():
     """Check is pyg data_loader is instantiated."""
-    test_dl = PointCloudDataset(dump_path, **DATALOADER_KWARGS)
-    assert len(test_dl) == 1
+    with TemporaryDirectory() as tmpdir:
+        test_dl = PointCloudDataset(Path(tmpdir), **DATALOADER_KWARGS)
+        assert len(test_dl) == 1
