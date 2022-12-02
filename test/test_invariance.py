5b635b35d8d93972b3c802e96af03ba3074f2e48
jscant <jack.scantlebury@gmail.com>
2022-12-02 13:41:19 +0100

    Switch to TemporaryDirectory for unit tests.

--- test/test_invariance.py
+++ test/test_invariance.py
@@ -1,34 +1,37 @@
+"""Tests for invariance to SE(3) transformations of the input."""
+
 import pytest
 import torch
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 from point_vs.models.geometric.egnn_lucid import PygLucidEGNN
 from point_vs.models.geometric.egnn_satorras import SartorrasEGNN
+from point_vs.global_objects import DEVICE
 
 from .setup_and_params import MODEL_KWARGS
 from .setup_and_params import ORIGINAL_GRAPH
 from .setup_and_params import EGNN_EPS
 from .setup_and_params import ROTATED_GRAPH
-from .setup_and_params import setup
-from .setup_and_params import DEVICE
-
-dump_path = setup()
 
 
 def test_lucid_egnn_invariance():
     """Check that lucidrains-based network is invariant to SE(3) transformations."""
-    model = PygLucidEGNN(
-        dump_path, 0, 0, None, None, **MODEL_KWARGS).to(DEVICE).eval()
+    with TemporaryDirectory() as tmpdir:
+        model = PygLucidEGNN(
+            Path(tmpdir), 0, 0, None, None, **MODEL_KWARGS).to(DEVICE).eval()
 
-    unrotated_result = float(torch.sigmoid(model(ORIGINAL_GRAPH)))
-    rotated_result = float(torch.sigmoid(model(ROTATED_GRAPH)))
-    assert unrotated_result == pytest.approx(rotated_result, abs=EGNN_EPS)
+        unrotated_result = float(torch.sigmoid(model(ORIGINAL_GRAPH)))
+        rotated_result = float(torch.sigmoid(model(ROTATED_GRAPH)))
+        assert unrotated_result == pytest.approx(rotated_result, abs=EGNN_EPS)
 
 
 def test_sartorras_egnn_invariance():
     """Check that satorras-egnn-based network is invariant to SE(3) transformations."""
-    model = SartorrasEGNN(
-        dump_path, 0, 0, None, None, **MODEL_KWARGS).to(DEVICE).eval()
+    with TemporaryDirectory() as tmpdir:
+        model = SartorrasEGNN(
+            Path(tmpdir), 0, 0, None, None, **MODEL_KWARGS).to(DEVICE).eval()
 
-    unrotated_result = float(torch.sigmoid(model(ORIGINAL_GRAPH)))
-    rotated_result = float(torch.sigmoid(model(ROTATED_GRAPH)))
-    assert unrotated_result == pytest.approx(rotated_result, abs=EGNN_EPS)
+        unrotated_result = float(torch.sigmoid(model(ORIGINAL_GRAPH)))
+        rotated_result = float(torch.sigmoid(model(ROTATED_GRAPH)))
+        assert unrotated_result == pytest.approx(rotated_result, abs=EGNN_EPS)
