dad590d4f66fa6dacc99d57e4cfa06e63dfcc92a
jscant <jack.scantlebury@gmail.com>
2022-12-22 14:54:54 +0000

    Formatting for pnn vanilla base.

--- point_vs/models/vanilla/pnn_vanilla_base.py
+++ point_vs/models/vanilla/pnn_vanilla_base.py
@@ -1,3 +1,5 @@
+"""Base class for non-pyg models."""
+
 from abc import ABC
 
 from point_vs.models.point_neural_network_base import PointNeuralNetworkBase
@@ -6,8 +8,8 @@ from point_vs.models.point_neural_network_base import PointNeuralNetworkBase
 class PNNVanillaBase(PointNeuralNetworkBase, ABC):
     """Base (abstract) class for non-geometric point cloud classifiers."""
 
-    def unpack_input_data_and_predict(self, inputs):
-        x, y_true, ligands, receptors = inputs
-        x = self.prepare_input(x)
-        y_pred = self(x).reshape(-1, )
+    def unpack_input_data_and_predict(self, input_data):
+        feats, y_true, ligands, receptors = input_data
+        feats = self.prepare_input(feats)
+        y_pred = self(feats).reshape(-1, )
         return y_pred, y_true, ligands, receptors
